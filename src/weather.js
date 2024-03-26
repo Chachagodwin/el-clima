be1f67c5aa0612645f2cfda35d30d242f384c9b1
Chachagodwin <charityokechukwucha@gmail.com>
2024-03-25 22:57:46 -0700

    Added the basic search engine without API

--- src/weather.js
+++ src/weather.js
@@ -1,1 +1,10 @@
-alert("Fuck you");
+function handleSearchSubmit(event) {
+    event.preventDefault();
+    let searchInput = document.querySelector("#search-form-input");
+    let locationElement = document.querySelector("#location");
+    locationElement.innerHTML = searchInput.value;
+}
+
+
+let searchFormElement = document.querySelector("#search-form");
+searchFormElement.addEventListener("submit", handleSearchSubmit);
